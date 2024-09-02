05fe966b3504ef99fe2f7e1e5271575a3c236ba8
Steven Gubkin <stevengubkin@Steve-2024-Macbook.local>
2024-09-02 12:32:46 -0400

    updating secret code

--- computer_setup_day/pressure_testing.py
+++ computer_setup_day/pressure_testing.py
@@ -1,17 +1,13 @@
+from importlib.metadata import version
 import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import sklearn
-import pyjokes
 
 from sklearn.linear_model import LinearRegression
 
 def secret_code():
-    z = int(np.__version__.split('.')[0]) 
-    z += 10*int(np.__version__.split('.')[0]) 
-    z += 100*int(mpl.__version__.split('.')[0])
-    z += 1000*int(sklearn.__version__.split('.')[0])
-    z += 10000*int(pyjokes.__version__.split('.')[1])
+    z = int(version('numpy').split('.')[0]) 
+    z += 10*int(version('pandas').split('.')[0]) 
+    z += 100*int(version('matplotlib').split('.')[0])
+    z += 1000*int(version('pyjokes').split('.')[1])
     X = np.loadtxt("X.txt", delimiter=",")
     lr = LinearRegression()
     lr.fit(X[:,:2],X[:,2])
